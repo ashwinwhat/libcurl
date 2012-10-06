17cf71517a708b381e82e4845b3c39c55596576a
Ashwin <ashwinve@thoughtworks.com>
2012-10-06 16:41:46 +0530

    [Ashwin]Added metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "ashwin.what@gmail.com"
 license "Apache 2.0"
 description "Installs libcurl from source"
 version "1.0.0"
-recipe "libcurl"
+recipe "libcurl", "Install libcurl"
 
 depends "build-essential"
